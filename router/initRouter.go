051fd630d13ad582a9a2783776b1b5fcd1e33222
Faisal Hari Salam <faisalharie@gmail.com>
2024-03-07 13:47:17 +0700

    FEAT : Doctor Schedule

--- router/initRouter.go
+++ router/initRouter.go
@@ -1,10 +1,16 @@
 package router
 
 import (
+	"avengers-clinic/src/doctorSchedule/doctorScheduleDelivery"
+	"avengers-clinic/src/doctorSchedule/doctorScheduleRepository"
+	"avengers-clinic/src/doctorSchedule/doctorScheduleUsecase"
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoute(v1Group *gin.RouterGroup, db *sql.DB) {
+	scheduleRepo := doctorScheduleRepository.NewDoctorScheduleRepo(db)
+	scheduleUC := doctorScheduleUsecase.NewDoctorScheduleUsecase(scheduleRepo)
+	doctorScheduleDelivery.NewDoctorScheduleDelivery(v1Group, scheduleUC)
 }
