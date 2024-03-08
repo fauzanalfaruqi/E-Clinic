e2ba7f82b77aa7cc975ed692ba33297e823ed618
Muhammad David Beckam <muhammaddavid1650@gmail.com>
2024-03-08 14:15:42 +0700

    [ADD] menambahkan fitur CRUD dan banyak lainnya

--- model/dto/userDto/userDto.go
+++ model/dto/userDto/userDto.go
@@ -8,6 +8,7 @@ type User struct {
 	Specialization interface{} `json:"specialization,omitempty"`
 	CreatedAt      string      `json:"created_at,omitempty"`
 	UpdatedAt      string      `json:"updated_at,omitempty"`
+	DeletedAt      string      `json:"deleted_at,omitempty"`
 }
 
 type AuthRequest struct {
@@ -18,10 +19,23 @@ type AuthRequest struct {
 type RegisterRequest struct {
 	Username       string      `json:"username" validate:"required"`
 	Password       string      `json:"password" validate:"required"`
-	Role           string      `json:"role" validate:"required"`
+	Role           string      `json:"role" validate:"required,enum=ADMIN DOCTOR PATIENT"`
 	Specialization interface{} `json:"specialization"`
 }
 
 type LoginResponse struct {
 	Token string `json:"token"`
+}
+
+type UpdateRequest struct {
+	ID             string      `json:"id"`
+	Username       string      `json:"username"`
+	Specialization interface{} `json:"specialization"`
+}
+
+type UpdatePasswordRequest struct {
+	ID                   string `json:"id"`
+	CurrentPassword      string `json:"current_password"`
+	NewPassword          string `json:"new_password"`
+	ConfirmationPassword string `json:"confirmation_password"`
 }
--- src/user/userInterface.go
+++ src/user/userInterface.go
@@ -3,14 +3,29 @@ package user
 import "avengers-clinic/model/dto/userDto"
 
 type UserRepository interface {
+	GetAllTrash() ([]userDto.User, error)
+	GetAll() ([]userDto.User, error)
 	GetByID(userID string) (userDto.User, error)
 	GetByUsername(username string) (userDto.User, error)
 	Insert(user userDto.User) (string, error)
+	Update(user userDto.User) error
+	UpdatePassword(userId, hashPassword string) error
+	Delete(userID string) error
+	SoftDelete(userID string) error
+	Restore(userID string) error
 	IsUsernameExists(username string) bool
 }
 
 type UserUsecase interface {
+	GetAllTrash() ([]userDto.User, error)
+	GetAll() ([]userDto.User, error)
+	GetByID(userID string) (userDto.User, error)
 	PatientRegister(req userDto.AuthRequest) (userDto.User, error)
 	UserRegister(req userDto.RegisterRequest) (userDto.User, error)
 	Login(req userDto.AuthRequest) (userDto.LoginResponse, error)
+	Update(req userDto.UpdateRequest) (userDto.User, error)
+	UpdatePassword(req userDto.UpdatePasswordRequest) error
+	Delete(userID string) error
+	SoftDelete(userID string) error
+	Restore(userID string) error
 }
