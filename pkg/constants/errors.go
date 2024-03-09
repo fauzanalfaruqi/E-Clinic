fa32b4b1e12adbc92f6cba663a57d825a29a84b0
fauzanalfaruqi <fauzanalfaruqi@gmail.com>
2024-03-09 12:37:19 +0700

    [FEAT] Add error constants related to update payment status

--- pkg/constants/errors.go
+++ pkg/constants/errors.go
@@ -1,7 +1,9 @@
 package constants
 
 const (
-	ErrScheduleTaken      = "doctors schedule at that time has been taken"
-	ErrScheduleNotMatch   = "your booking isn't match with doctor's schedule"
-	ErrPaymentAlreadyTrue = "the payment has already been set to true"
+	ErrScheduleTaken            = "doctors schedule at that time has been taken"
+	ErrScheduleNotMatch         = "your booking isn't match with doctor's schedule"
+	ErrPaymentAlreadyTrue       = "the payment has already been set to true"
+	ErrQuantityGreaterThanStock = "quantity amount is greater than the stock available"
+	ErrNoStockAvailable         = "no stock available for this item"
 )
