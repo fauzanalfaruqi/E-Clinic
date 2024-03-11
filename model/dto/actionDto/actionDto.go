199aa70caddeaff8465891465d1325cb5b462823
Muhammad David Beckam <muhammaddavid1650@gmail.com>
2024-03-12 05:50:27 +0700

    [ADD] add file actionRepository_test.go

--- model/dto/actionDto/actionDto.go
+++ model/dto/actionDto/actionDto.go
@@ -1,19 +1,19 @@
 package actionDto
 
 type Action struct {
-	ID          string `json:"id,omitempty"`
-	Name        string `json:"name,omitempty"`
-	Price       int    `json:"price,omitempty"`
-	Description string `json:"description,omitempty"`
-	CreatedAt   string `json:"created_at,omitempty"`
-	UpdatedAt   string `json:"updated_at,omitempty"`
-	DeletedAt   string `json:"deleted_at,omitempty"`
+	ID          string      `json:"id,omitempty"`
+	Name        string      `json:"name,omitempty"`
+	Price       int         `json:"price,omitempty"`
+	Description interface{} `json:"description,omitempty"`
+	CreatedAt   string      `json:"created_at,omitempty"`
+	UpdatedAt   string      `json:"updated_at,omitempty"`
+	DeletedAt   string      `json:"deleted_at,omitempty"`
 }
 
 type CreateRequest struct {
-	Name        string `json:"name" validate:"required"`
-	Price       int    `json:"price" validate:"required,number"`
-	Description string `json:"description"`
+	Name        string      `json:"name" validate:"required"`
+	Price       int         `json:"price" validate:"required,number"`
+	Description interface{} `json:"description"`
 }
 
 type UpdateRequest struct {
