7f3b13cc62a85cc13e610bcf5851c7db2c49ff00
Faisal Hari Salam <faisalharie@gmail.com>
2024-03-13 06:25:51 +0700

    FIX: adjusting table mst_schedule_time, doctor_schedule & bookings

--- DDL.sql
+++ DDL.sql
@@ -20,9 +20,9 @@ CREATE TABLE users (
 CREATE TABLE doctor_schedules (
   id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   doctor_id uuid NOT NULL REFERENCES users (id),
-  day_of_week INT NOT NULL,
-  start_at TIME NOT NULL,
-  end_at TIME NOT NULL,
+  schedule_date DATE NOT NULL,
+  start_at INT NOT NULL REFERENCES mst_schedule_time(id),
+  end_at INT NOT NULL REFERENCES mst_schedule_time(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP
@@ -51,11 +51,10 @@ CREATE TABLE actions (
 );
 
 CREATE TABLE bookings (
-  id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   patient_id uuid NOT NULL REFERENCES users (id),
   doctor_schedule_id uuid NOT NULL REFERENCES doctor_schedules(id),
-  booking_date DATE NOT NULL,
-  mst_schedule_id int NOT NULL REFERENCES mst_schedule(id),
+  mst_schedule_id int NOT NULL REFERENCES mst_schedule_time(id),
   status booking_status NOT NULL,
   complaint text NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -98,11 +97,11 @@ CREATE TABLE medical_record_action_details (
 );
 
 
-CREATE TABLE mst_schedule(
+CREATE TABLE mst_schedule_time(
   id INT PRIMARY KEY,
   start_at TIME NOT NULL,
   end_at TIME NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP
-)
+);
--- DMLFeatSched.sql
+++ DMLFeatSched.sql
@@ -1,11 +1,13 @@
 INSERT INTO users(id username, password, role, specialization) VALUES('67b65471-eb1f-46ec-a043-959a5cc85778', 'urip', 'aaadasadadasaa', 'PATIENT', ''), ('5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', 'martono', 'aaadasadadasaa', 'DOCTOR', 'ortopedi');
 
 
-INSERT INTO doctor_schedules(id, doctor_id, day_of_week, start_at, end_at) VALUES('74d93144-6f2e-4bbc-9f89-973c62d3ac54', '5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', 1, '09:00:00', '15:00:00'), ('5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', 2, '09:00:00', '15:00:00'), ('a98528e4-3fa4-48e5-adaa-14fb3a21d981','5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', 4, '09:00:00', '15:00:00')
-
-INSERT INTO mst_schedule(id, start_at, end_at) VALUES (1, '08:00:30', '08:30:00'), (2, '08:30:30', '09:00:00'), (3, '09:00:30', '09:30:00'), (4, '09:30:30', '10:00:00'), (5, '10:00:30', '10:30:00'), (6, '10:30:30', '11:00:00'), (7, '11:00:30', '11:30:00'), (8, '11:30:30', '12:00:00'), (9, '13:00:30', '13:30:00'), (10, '13:30:30', '14:00:00'), (11, '14:00:30', '14:30:00'), (12, '14:30:30', '15:00:00'), (13, '15:00:30', '15:30:00'), (14, '15:30:30', '16:00:00');
+INSERT INTO mst_schedule_time(id, start_at, end_at) VALUES (1, '08:00:30', '08:30:00'), (2, '08:30:30', '09:00:00'), (3, '09:00:30', '09:30:00'), (4, '09:30:30', '10:00:00'), (5, '10:00:30', '10:30:00'), (6, '10:30:30', '11:00:00'), (7, '11:00:30', '11:30:00'), (8, '11:30:30', '12:00:00'), (9, '13:00:30', '13:30:00'), (10, '13:30:30', '14:00:00'), (11, '14:00:30', '14:30:00'), (12, '14:30:30', '15:00:00'), (13, '15:00:30', '15:30:00'), (14, '15:30:30', '16:00:00');
 
 
-INSERT INTO bookings(doctor_schedule_id,patient_id, booking_date, mst_schedule_id, complaint, status) VALUES ('74d93144-6f2e-4bbc-9f89-973c62d3ac54','67b65471-eb1f-46ec-a043-959a5cc85778', '2024-12-12', 1,'sakit perut melilit kek mau mateee', 'WAITING'), ('74d93144-6f2e-4bbc-9f89-973c62d3ac54','67b65471-eb1f-46ec-a043-959a5cc85778', '2024-01-12', 1,'sakit kepala dihantam realita', 'CANCELED');
+INSERT INTO doctor_schedules(id, doctor_id, schedule_date, start_at, end_at) VALUES('74d93144-6f2e-4bbc-9f89-973c62d3ac54', '5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', '2024-03-14', 1, 8), ('e076f890-0ed3-4df2-bb57-e9dc6e48fc89', '5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', '2024-03-15', 1, 9), ('a98528e4-3fa4-48e5-adaa-14fb3a21d981','5bc18dd0-58cb-4612-8dc3-5fc2419b7f29', '2024-03-16', 1, 9);
+
+
+
+INSERT INTO bookings(doctor_schedule_id,patient_id, mst_schedule_id, complaint, status) VALUES ('74d93144-6f2e-4bbc-9f89-973c62d3ac54','67b65471-eb1f-46ec-a043-959a5cc85778', 1,'sakit perut melilit kek mau mateee', 'WAITING'), ('74d93144-6f2e-4bbc-9f89-973c62d3ac54','67b65471-eb1f-46ec-a043-959a5cc85778', 2,'sakit kepala dihantam realita', 'CANCELED');
 
 
