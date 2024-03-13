8633d94bd8fbe78be6d187f6df5a8b5646fc1c20
Faisal Hari Salam <faisalharie@gmail.com>
2024-03-13 23:27:55 +0700

    FIX: Conflict merge development

--- pkg/constants/errors.go
+++ pkg/constants/errors.go
@@ -1,18 +1,12 @@
 package constants
 
 const (
-<<<<<<< HEAD
-	ErrScheduleTaken = "doctors schedule at that time has been taken"
-	ErrScheduleNotMatch = "your booking isn't match with doctor's schedule"
-	ErrScheduleDateExist = "the schedule for that date already exists"
-	ErrDateFormat = "invalid date format"
-	ErrDocSchedNotExist = "doctor schedule is not exist"
-)
-=======
 	ErrScheduleTaken            = "doctors schedule at that time has been taken"
 	ErrScheduleNotMatch         = "your booking isn't match with doctor's schedule"
+	ErrScheduleDateExist        = "the schedule for that date already exists"
+	ErrDateFormat               = "invalid date format"
+	ErrDocSchedNotExist         = "doctor schedule is not exist"
 	ErrPaymentAlreadyTrue       = "the payment has already been set to true"
 	ErrQuantityGreaterThanStock = "quantity amount is greater than the stock available"
 	ErrNoStockAvailable         = "no stock available for this item"
 )
->>>>>>> 5e14e35dcd871b901540d4f2a15690620abde368
